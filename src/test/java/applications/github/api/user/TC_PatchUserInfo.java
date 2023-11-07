16a21459238f2155bb964d38e930133758c03cfd
lkomon <lukaszkomon@gmail.com>
2023-11-07 15:50:40 +0100

    MODIFY: add report folder for logs, add timestamp in report name, add logging in TC

--- src/test/java/applications/github/api/user/TC_PatchUserInfo.java
+++ src/test/java/applications/github/api/user/TC_PatchUserInfo.java
@@ -3,6 +3,8 @@ package applications.github.api.user;
 import applications.github.api.user_module.models.User;
 import applications.github.api.user_module.services.UserService;
 import config.github.api.Config;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 import utilities.jsonUtils.JsonUtils;
@@ -13,14 +15,20 @@ import static org.hamcrest.Matchers.samePropertyValuesAs;
 import static utilities.requestsUtils.HeaderConfigs.createHeadersFromProperties;
 
 public class TC_PatchUserInfo extends Config {
+    public static final Logger _logger = LogManager.getLogger(Config.class);
+
     // Positive scenario
     @Test
     @Parameters({"updatedUserData","headersPath","userTokenPath"})
     public void assertThatAuthenticatedUserBioIsUpdated(String updatedUserDataJsonPath, String headersPath, String userTokenPath) throws AutomationException {
+        _logger.info("Perform test that check if after PATCH request data is changed");
         User newUserData = JsonUtils.jsonToObject(updatedUserDataJsonPath, User.class);
         User patchUser = (User) UserService.init().
                 patchUser(updatedUserDataJsonPath, createHeadersFromProperties(new String[] {headersPath,userTokenPath})).
                 getResponse();
+        _logger.info("NEW USER BIO:" + newUserData.getBio());
+        _logger.info("USER BIO AFTER PATCH:" + patchUser.getBio());
         assertThat(newUserData.getBio() ,samePropertyValuesAs(patchUser.getBio()));
+
     }
 }
